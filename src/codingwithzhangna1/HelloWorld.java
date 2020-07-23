05a62779831dbd7b92addc794a7b3f076cf94e42
zhangna <907404296@qq.com>
2020-07-23 14:29:29 +0800

    init

---
+++ src/codingwithzhangna1/HelloWorld.java
@@ -0,0 +1,17 @@
+package codingwithzhangna1;
+
+public class HelloWorld {
+
+    public static void main(String[] args){
+        System.out.println("Hello World!!!!");
+        System.out.println("Hello World!!!!");
+        System.out.println("Hello World!!!!");
+        System.out.println("Hello World!!!!");
+        aa();
+        System.out.println("Hello World!!!!");
+    }
+
+    public static void aa() {
+        System.out.println("aa");
+    }
+}
